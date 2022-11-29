591542e1b21dc9a59e83f03e4fbf32ae4c97e287
yabuki829 <syodai829@icloud.com>
2022-11-29 16:15:59 +0900

    Initial Commit

---
+++ infinity-scroll/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  infinity-scroll
+//
+//  Created by 薮木翔大 on 2022/11/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
